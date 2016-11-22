3acb4ffc154c75a6b8427bf193b0d3c8e1f019d0
Thewarit chompoo <thewarit1513@gmail.com>
2016-11-22 14:16:08 +0700

    newfile .travis.yml

--- Database.java
+++
@@ -1,9 +0,0 @@
-package th.ac.crru.ci;
-
-public interface Database {
-	public String connectDb(String connString);
-	public String insertTable(String sql);
-	public String updateTable(String sql);
-	public String deleteTable(String sql);
-	public String disconnectDb();
-}
